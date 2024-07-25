7d83b9942bcb7660fb299dcb21db97987b69a934
Laila <lvanrooijen@live.nl>
2024-07-25 18:37:18 +0200

    Failure is a proper teacher

--- build-a-bear-frontend/src/pages/Register.tsx
+++ build-a-bear-frontend/src/pages/Register.tsx
@@ -2,20 +2,19 @@ import { useNavigate } from "react-router-dom";
 import Button from "../components/Button";
 import FieldInput from "../components/FieldInput";
 import Page from "../components/Page";
-
-const Login = () => {
+const Register = () => {
   const navigate = useNavigate();
   return (
     <Page style={"flex flex-row justify-center items-center"}>
-      <div className="bg-maize text-purple rounded-lg">
+      <div className="bg-maize rounded-lg w-fit text-purple">
         <h2 className="font-extrabold text-xl bg-purple text-maize p-2 border-t-2 border-x-2 border-maize min-h-16 flex flex-row items-center justify-center">
-          Login
+          Register
         </h2>
         <div className="w-[500px]">
           <FieldInput
             label={"email"}
             content={""}
-            style={"space-x-10 my-3  mx-4"}
+            style={"space-x-10 my-3 mx-4"}
             bgAndTxt={"bg-purple text-maize"}
             handleChange={(value) => {
               console.log("username: ", value);
@@ -23,6 +22,15 @@ const Login = () => {
           />
           <FieldInput
             label={"password"}
+            content={""}
+            style={"space-x-10 my-3  mx-4"}
+            bgAndTxt={"bg-purple text-maize"}
+            handleChange={(value) => {
+              console.log("password: ", value);
+            }}
+          />
+          <FieldInput
+            label={"comfirm password"}
             content={""}
             style={"space-x-10 my-3 mx-4"}
             bgAndTxt={"bg-purple text-maize"}
@@ -38,16 +46,16 @@ const Login = () => {
             style={"w-30 self-center"}
           />
         </div>
-        <div className="py-4 px-2 text-center font-bold">
+        <div className="p-2 text-center font-bold">
           <p>
-            Don't have an account? {""}
+            Already have an account? {""}
             <span
               onClick={() => {
-                navigate("/register");
+                navigate("/login");
               }}
               className="font-extrabold text-lg border-b-2 border-purple cursor-pointer"
             >
-              Sign up
+              Sign in
             </span>
           </p>
         </div>
@@ -56,4 +64,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
